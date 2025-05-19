a776cae5c39337c009570609ace077186752254b
M1CH4L69 <161817711+M1CH4L69@users.noreply.github.com>
2025-05-19 15:29:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -221,4 +221,4 @@ def change_password():
     return render_template("change_password.html", error=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='X.X.X.X', port = XXXX)
